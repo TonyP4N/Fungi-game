958b41539121243d248d54cc70079675f93c23ba
EaSy_ <TP3_0805@outlook.com>
2022-04-16 01:36:56 +0100

    v1.0

--- src/board/Player.java
+++ src/board/Player.java
@@ -69,9 +69,7 @@ public class Player {
         display.add(cards);
     }
 
-    public boolean takeCardFromTheForest(int index) {
-        return false;
-    }
+    public boolean takeCardFromTheForest(int index) {return false;}
 
     public boolean takeFromDecay() {
         return false;
